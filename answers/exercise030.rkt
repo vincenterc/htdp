754190a71ff735023dacf9ed001f54521f4f6593
vincenter <vincenterr@yahoo.com.tw>
2024-11-19 17:28:21 +0800

    2.4

---
+++ answers/exercise030.rkt
@@ -0,0 +1,26 @@
+#lang htdp/bsl
+
+(define BASE-PRICE 5.0)
+(define BASE-ATTENDEES 120)
+
+(define PRICE-ATTENDANCE (/ 15 0.1))
+
+(define PERFORMANCE-COST 180)
+(define ATTENDEE-COST 0.04)
+
+(define (attendees ticket-price)
+  (- BASE-ATTENDEES (* (- ticket-price BASE-PRICE) PRICE-ATTENDANCE)))
+
+(define (revenue ticket-price)
+  (* ticket-price (attendees ticket-price)))
+
+(define (cost ticket-price)
+  (+ PERFORMANCE-COST (* ATTENDEE-COST (attendees ticket-price))))
+
+(define (profit ticket-price)
+  (- (revenue ticket-price)
+     (cost ticket-price)))
+
+(profit 3) ; 1063.2
+(profit 4) ; 889.2
+(profit 5) ; 415.2
