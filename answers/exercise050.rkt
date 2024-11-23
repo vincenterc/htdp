dd4f074a7a86643650a4bc43a8b7336176edb937
vincenter <vincenterr@yahoo.com.tw>
2024-11-23 13:10:06 +0800

    4.3

---
+++ answers/exercise050.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise050) (read-case-sensitive #true) (teachpacks ()) (htdp-settings #(#true constructor repeating-decimal #false #true none #false () #false)))
+
+; TrafficLight -> TrafficLight
+; yields the next state given current state s
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
---
+++ answers/exercise051.rkt
@@ -0,0 +1,35 @@
+#lang htdp/bsl
+
+(require 2htdp/universe)
+(require 2htdp/image)
+
+; A TrafficLight is one of the following Strings:
+; – "red"
+; – "green"
+; – "yellow"
+; interpretation the three strings represent the three
+; possible states that a traffic light may assume
+
+(define INITIAL-STATE "red")
+
+; Number -> Image
+; simulates a traffic light for a given duration
+(define (main duration)
+  (big-bang INITIAL-STATE
+    [on-tick traffic-light-next 1 duration]
+    [to-draw render]))
+
+; TrafficLight -> TrafficLight
+; yields the next state given current state s
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
+
+; TrafficLight -> Image
+(define (render s)
+  (circle 20 "solid" s))
