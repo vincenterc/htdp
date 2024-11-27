de8389546c01ccbdede5a5ed6a0af9a272dbbc72
vincenter <vincenterr@yahoo.com.tw>
2024-11-27 21:54:17 +0800

    5.4

---
+++ answers/exercise067.rkt
@@ -0,0 +1,5 @@
+#lang htdp/bsl
+
+(define SPEED 3)
+(define-struct balld [location direction])
+(make-balld 10 "up")
---
+++ answers/exercise068.rkt
@@ -0,0 +1,4 @@
+#lang htdp/bsl
+
+(define-struct ballf [x y deltax deltay])
+(make-ballf 30 40 -10 5)
