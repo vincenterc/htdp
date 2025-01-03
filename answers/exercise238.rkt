97cd009945c1cdba71f4e345481b7652af7c6269
vincenter <vincenterr@yahoo.com.tw>
2025-01-03 16:37:31 +0800

    Modify exercise238

--- answers/exercise238.rkt
+++ answers/exercise238.rkt
@@ -1,5 +1,10 @@
 #lang htdp/isl
 
+(define list-1 (list 25 24 23 22 21 20 19 18 17 16 15 14 13
+                     12 11 10 9 8 7 6 5 4 3 2 1))
+(define list-2 (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+                     17 18 19 20 21 22 23 24 25))
+
 ; Nelon -> Number
 ; determines the smallest
 ; number on l
@@ -40,19 +45,15 @@
 ; determines the smallest number on l
 (define (inf-1 l)
   (extremum < l))
-; (inf-1 (list 25 24 23 22 21 20 19 18 17 16 15 14 13
-;       12 11 10 9 8 7 6 5 4 3 2 1))
-; (inf-1 (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-;              17 18 19 20 21 22 23 24 25))
+; (inf-1 list-1)
+; (inf-1 list-2)
 
 ; Nelon -> Number
 ; determines the largest number on l
 (define (sup-1 l)
   (extremum > l))
-; (sup-1 (list 25 24 23 22 21 20 19 18 17 16 15 14 13
-;              12 11 10 9 8 7 6 5 4 3 2 1))
-; (sup-1 (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-;              17 18 19 20 21 22 23 24 25))
+; (sup-1 list-1)
+; (sup-1 list-2)
 
 ; Nelon -> Number
 ; determines the smallest
@@ -88,16 +89,12 @@
 ; determines the smallest number on l
 (define (inf-2 l)
   (extremum-2 min l))
-; (inf-2 (list 25 24 23 22 21 20 19 18 17 16 15 14 13
-;       12 11 10 9 8 7 6 5 4 3 2 1))
-; (inf-2 (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-;              17 18 19 20 21 22 23 24 25))
+; (inf-2 list-1)
+; (inf-2 list-2)
 
 ; Nelon -> Number
 ; determines the largest number on l
 (define (sup-2 l)
   (extremum-2 max l))
-; (sup-2 (list 25 24 23 22 21 20 19 18 17 16 15 14 13
-;              12 11 10 9 8 7 6 5 4 3 2 1))
-; (sup-2 (list 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
-;              17 18 19 20 21 22 23 24 25))
+; (sup-2 list-1)
+; (sup-2 list-2)
