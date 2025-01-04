c7c16a4e7a979135ef2d18670d920be3a6d25632
vincenter <vincenterr@yahoo.com.tw>
2025-01-05 03:26:39 +0800

    Modify exercise269

--- answers/exercise269.rkt
+++ answers/exercise269.rkt
@@ -1,46 +1,43 @@
 #lang htdp/isl+
 
-(define-struct ir [name description cost price])
+(define-struct IR [name price])
 ; An IR is a structure:
-;   (make-ir String String Number Number)
-; interpretation (make-ir n d c p) specifies the name of an item n
-; ,a description d, the acquisition price c,
-; and the recommended sales price p
+;   (make-IR String Number)
 
 (define list-1
-  (list (make-ir "doll" "doll" 20 21)
-        (make-ir "bear" "bear" 9 13)
-        (make-ir "cat" "cat" 4 7)
-        (make-ir "dog" "dog" 5 8)))
+  (list (make-IR "doll" 21)
+        (make-IR "bear" 13)
+        (make-IR "cat" 7)
+        (make-IR "dog" 8)))
 
 ; [List-of IR] -> [List-of IR]
 ; produces a list of inventory records whose sales price is below ua
-; given l
+; from the list l
 (check-expect (eliminate-expensive 15 list-1)
-              (list (make-ir "bear" "bear" 9 13)
-                    (make-ir "cat" "cat" 4 7)
-                    (make-ir "dog" "dog" 5 8)))
+              (list (make-IR "bear" 13)
+                    (make-IR "cat" 7)
+                    (make-IR "dog" 8)))
 (define (eliminate-expensive ua l)
   (local (; [IR] -> Boolean
-          ; determines whether the sales price of i
+          ; determines whether the sales price of ir
           ; is below ua
-          (define (below-ua? i)
-            (< (ir-price i) ua)))
+          (define (below-ua? ir)
+            (< (IR-price ir) ua)))
     (filter below-ua? l)))
 
 ; [List-of IR] -> [List-of IR]
 ; produces a list of inventory records that do not use the name ty
-; given l
+; from the list l
 (check-expect (recall "bear" list-1)
-              (list (make-ir "doll" "doll" 20 21)
-                    (make-ir "cat" "cat" 4 7)
-                    (make-ir "dog" "dog" 5 8)))
+              (list (make-IR "doll" 21)
+                    (make-IR "cat" 7)
+                    (make-IR "dog" 8)))
 (define (recall ty l)
   (local (; [IR -> Boolean]
-          ; determines whether the name of the given inventory item i
+          ; determines whether the name of the given inventory item ir
           ; is not the same of ty
-          (define (not-name-ty? i)
-            (not (string=? (ir-name i) ty))))
+          (define (not-name-ty? ir)
+            (not (string=? (IR-name ir) ty))))
     (filter not-name-ty? l)))
 
 (define LN-1 (list "a" "b" "c" "d"))
