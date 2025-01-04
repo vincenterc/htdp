5e18bf76acb08e9094f65a0699793444d5c07c08
vincenter <vincenterr@yahoo.com.tw>
2025-01-04 21:17:37 +0800

    Modify exercise190

--- answers/exercise190.rkt
+++ answers/exercise190.rkt
@@ -8,40 +8,47 @@
 ; – '()
 ; – (cons Lo1s Llo1s)
 
+(define list-a (list "a"))
+(define list-b (list "b"))
+(define list-c (list "c"))
+(define list-a-b (list "a" "b"))
+(define list-b-c (list "b" "c"))
+(define list-a-b-c (list "a" "b" "c"))
+
 ; Lo1s -> Llo1s
 ; produces the list of all prefixes given l
 (check-expect (prefixes '()) '())
-(check-expect (prefixes (list 1))
-              (list (list 1)))
-(check-expect (prefixes (list 1 2))
-              (list (list 1 2) (list 1)))
-(check-expect (prefixes (list 1 2 3))
-              (list (list 1 2 3) (list 1 2) (list 1)))
+(check-expect (prefixes list-a)
+              (list list-a))
+(check-expect (prefixes list-a-b)
+              (list list-a-b list-a))
+(check-expect (prefixes list-a-b-c)
+              (list list-a-b-c list-a-b list-a))
 (define (prefixes l)
   (cond [(empty? l) '()]
         [else (cons l (prefixes (remove-last l)))]))
 
 ; Lo1s -> Lo1s
 ; removes the last 1String in l
-(check-expect (remove-last (list 1))
+(check-expect (remove-last list-a)
               '())
-(check-expect (remove-last (list 1 2))
-              (list 1))
-(check-expect (remove-last (list 1 2 3))
-              (list 1 2))
+(check-expect (remove-last list-a-b)
+              list-a)
+(check-expect (remove-last list-a-b-c)
+              list-a-b)
 (define (remove-last l)
   (cond [(empty? (rest l)) '()]
         [else (cons (first l) (remove-last (rest l)))]))
 
-;; Lo1s -> Llo1s
+; Lo1s -> Llo1s
 ; produces the list of all suffixes given l
 (check-expect (suffixes '()) '())
-(check-expect (suffixes (list 1))
-              (list (list 1)))
-(check-expect (suffixes (list 1 2))
-              (list (list 1 2) (list 2)))
-(check-expect (suffixes (list 1 2 3))
-              (list (list 1 2 3) (list 2 3) (list 3)))
+(check-expect (suffixes list-a)
+              (list list-a))
+(check-expect (suffixes list-a-b)
+              (list list-a-b list-b))
+(check-expect (suffixes list-a-b-c)
+              (list list-a-b-c list-b-c list-c))
 (define (suffixes l)
   (cond [(empty? l) '()]
         [else (cons l (suffixes (rest l)))]))
