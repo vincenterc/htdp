a54090812c99e980c5918f2c8570407f2602a56b
vincenter <vincenterr@yahoo.com.tw>
2025-01-05 14:34:27 +0800

    Modify exercise 245

--- answers/exercise245.rkt
+++ answers/exercise245.rkt
@@ -4,3 +4,6 @@
   (and (= (f1 1.2) (f2 1.2))
        (= (f1 3) (f2 3))
        (= (f1 -5.775) (f2 -5.775))))
+
+; Can not define function=? because you can not try all inputs,
+; as there are not a finite number of them.
