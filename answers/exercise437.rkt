36572743da07a811b3a321cf780799696dce9c78
vincenter <vincenterr@yahoo.com.tw>
2025-01-31 11:31:40 +0800

    26.3

---
+++ answers/exercise437.rkt
@@ -0,0 +1,10 @@
+#lang htdp/isl+
+
+; (define (solve P) 0)
+; (define combine-solutions add1)
+
+; (define (solve P) '())
+; (define combine-solutions cons)
+
+; (define (solve P) '())
+; (define combine-solutions cons)
