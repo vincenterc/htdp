adf69d821ce558100c009f149e16fe43ec2c509d
vincenter <vincenterr@yahoo.com.tw>
2025-02-09 22:38:59 +0800

    31.2

---
+++ answers/exercise492.rkt
@@ -0,0 +1,82 @@
+#lang htdp/isl+
+
+; A Graph is [List-of NN]
+; An NN (Name and Neighbors) is a list of two items:
+;   (list Node [List-of Node])
+; interpretation the first item is the name of a node and
+; the second one contains its (immediate) neighbors
+; A Node is a Symbol.
+
+(define sample-graph
+  (list (list 'A (list 'B 'E))
+        (list 'B (list 'E 'F))
+        (list 'C (list 'D))
+        (list 'D '())
+        (list 'E (list 'C 'F))
+        (list 'F (list 'D 'G))
+        (list 'G '())))
+
+(define cyclic-graph
+  (list (list 'A (list 'B 'E))
+        (list 'B (list 'E 'F))
+        (list 'C (list 'B 'D))
+        (list 'D '())
+        (list 'E (list 'C 'F))
+        (list 'F (list 'D 'G))
+        (list 'G '())))
+
+; A Path is a [List-of Node].
+; interpretation The list of nodes specifies a sequence
+; of immediate neighbors that leads from the first
+; Node on the list to the last one.
+
+; Node Node Graph -> [Maybe Path]
+; finds a path from origination to destination in G
+; if there is no path, the function produces #false
+(check-expect (find-path 'C 'D sample-graph)
+              '(C D))
+(check-member-of (find-path 'E 'D sample-graph)
+                 '(E F D) '(E C D))
+(check-expect (find-path 'C 'G sample-graph)
+              #false)
+(check-expect (find-path 'A 'G sample-graph)
+              '(A B E F G))
+(check-expect (find-path 'B 'D cyclic-graph)
+              '(B E C D))
+(define (find-path origination destination G)
+  (local (; Node [List-Node] -> [Maybe Path]
+          (define (find-path-aux origin seen)
+            (cond
+              [(symbol=? origin destination) (list destination)]
+              [(member? origin seen) #false]
+              [else (local ((define next (neighbors origin G))
+                            (define candidate
+                              (find-path/list next (cons origin seen))))
+                      (cond
+                        [(boolean? candidate) #false]
+                        [else (cons origin candidate)]))]))
+          ; [List-of Node] Node Graph -> [Maybe Path]
+          ; finds a path from some node on lo-Os to D
+          ; if there is no path, the function produces #false
+          (define (find-path/list lo-Os seen)
+            (cond
+              [(empty? lo-Os) #false]
+              [else (local ((define candidate
+                              (find-path-aux (first lo-Os) seen)))
+                      (cond
+                        [(boolean? candidate)
+                         (find-path/list (rest lo-Os) seen)]
+                        [else candidate]))])))
+    (find-path-aux origination '())))
+
+; Node Graph -> [List-of Node]
+; finds the list of immediate neighbors of n in g
+(check-expect (neighbors 'A '()) '())
+(check-expect (neighbors 'A sample-graph) '(B E))
+(check-expect (neighbors 'D sample-graph) '())
+(define (neighbors n g)
+  (local (; [Maybe NN]
+          (define nn (assq n g)))
+    (if (boolean? nn)
+        '()
+        (second nn))))
